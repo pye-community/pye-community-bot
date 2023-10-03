4f6b2b5c3dc935f3eaef28a7eb4ab436169101f2
Cheree <71246795+Chere3@users.noreply.github.com>
2023-10-02 18:15:20 -0600

    build: change proyect structure and formatted the files names to camelCase

---
+++ src/modules/bot/eventsLoader.ts
@@ -0,0 +1,19 @@
+import path from "path";
+import { readdirSync } from "fs";
+import { Client } from "discord.js";
+
+/** @link https://discordjs.guide/creating-your-bot/event-handling.html#individual-event-files */
+export const loadEvents = (client: Client) => {
+  const eventsPath = path.join(__dirname, "events");
+  const eventsFiles = readdirSync(`${eventsPath}`).filter((file) =>
+    file.match(/\.(ts|js)$/)
+  );
+  for (const eventFile of eventsFiles) {
+    const event = require(`${eventsPath}/${eventFile}`);
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args, client));
+    }
+  }
+};
