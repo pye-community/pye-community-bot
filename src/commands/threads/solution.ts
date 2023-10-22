cc47af993923a5a7c32491c5a677dc14b27b7821
ChirujanoCodding <trdfabris@gmail.com>
2023-10-22 13:11:50 -0300

    refactor: :fire: removed eslint rules

--- src/commands/threads/solution.ts
+++ src/commands/threads/solution.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-
 import { ChatInputCommandInteraction } from 'discord.js';
 import { CommandBuilder } from './../../modules/bot/handlers';
 
